97b2ac175e0828207a70e16f1ec0f09da37591d8
codyallen9202 <codyman9202@gmail.com>
2023-12-07 16:07:51 -0500

    article page

--- src/pages/articlepage.js
+++ src/pages/articlepage.js
@@ -1,6 +1,14 @@
+import ArticleCard from "../components/article-card";
+
 function ArticlePage() {
     return (
-        <h1>article</h1>
+        <div className='article-container'>
+            <ArticleCard
+                title={'Adding Abreu takes big Sprint Car name off the board in favor of High Limit'}
+                image={'images/rico-24.JPG'} 
+                author={'Richard Allen'}
+            />
+        </div>
     );
 }
 
