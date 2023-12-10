f672fe9828562ebc92a74f96c40cf0fb6971d107
codyallen9202 <codyman9202@gmail.com>
2023-12-10 10:50:53 -0500

    mobile styling

--- src/components/footer-bar.js
+++ src/components/footer-bar.js
@@ -4,38 +4,40 @@ import '../styles/component-styles.css'
 function FooterBar() {
     return (
         <Navbar className='footer-bar' bg='dark'>
-            <div className='social-container'>
-                <Navbar.Brand href='https://www.facebook.com/people/Motorsports-Report/61553590751076/' target='_blank'>
-                    <img
-                        src='/images/facebook-logo.png'
-                        alt='Facebook'
-                        height={30}
-                    />
-                </Navbar.Brand>
-                <Navbar.Brand href='https://twitter.com/MtrsportsReport' target='_blank'>
-                    <img
-                        src='/images/twitter-logo.png'
-                        alt='Twitter'
-                        height={30}
-                    />
-                </Navbar.Brand>
-                <Navbar.Brand href='https://www.youtube.com/channel/UCAsojScNEyJPF3e7uxW6boQ' target='_blank'>
-                    <img
-                        src='/images/youtube-logo.png'
-                        alt='YouTube'
-                        height={30}
-                    />
-                </Navbar.Brand>
-                <Navbar.Brand href='https://instagram.com' target='_blank'>
-                    <img
-                        src='/images/instagram-logo.png'
-                        alt='Instagram'
-                        height={30}
-                    />
-                </Navbar.Brand>
-            </div>
-            <div className='site-info'>
-                <p>© 2023 MotorsportsReport.net || Created and Maintained by Cody Allen</p>
+            <div className='bar-wrapper'>
+                <div className='social-container'>
+                    <Navbar.Brand href='https://www.facebook.com/people/Motorsports-Report/61553590751076/' target='_blank'>
+                        <img
+                            src='/images/facebook-logo.png'
+                            alt='Facebook'
+                            height={30}
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Brand href='https://twitter.com/MtrsportsReport' target='_blank'>
+                        <img
+                            src='/images/twitter-logo.png'
+                            alt='Twitter'
+                            height={30}
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Brand href='https://www.youtube.com/channel/UCAsojScNEyJPF3e7uxW6boQ' target='_blank'>
+                        <img
+                            src='/images/youtube-logo.png'
+                            alt='YouTube'
+                            height={30}
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Brand href='https://instagram.com' target='_blank'>
+                        <img
+                            src='/images/instagram-logo.png'
+                            alt='Instagram'
+                            height={30}
+                        />
+                    </Navbar.Brand>
+                </div>
+                <div className='site-info'>
+                    <p>© 2023 MotorsportsReport.net || Created and Maintained by Cody Allen</p>
+                </div>
             </div>
         </Navbar>
     );
--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -5,20 +5,22 @@ import '../styles/component-styles.css'
 function HeaderBar() {
   return (
       <Navbar className='header-bar' bg='dark' fixed='top'>
-          <Navbar.Brand href="/home">
-            <img
-              alt="Motorsports Retport"
-              src={'/images/logo.png'}
-              height={70}
-            />
-          </Navbar.Brand>
-          <Nav>
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/DLM">Dirt Late Models</Nav.Link>
-            <Nav.Link href="/DSC">Sprint Cars</Nav.Link>
-            <Nav.Link href="/NASCAR">NASCAR</Nav.Link>
-            <Nav.Link href="/other">Other</Nav.Link>
-          </Nav>
+          <div className='bar-wrapper'>
+            <Navbar.Brand href="/home">
+              <img
+                alt="Motorsports Retport"
+                src={'/images/logo.png'}
+                height={70}
+              />
+            </Navbar.Brand>
+            <Nav>
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link href="/DLM">Dirt Late Models</Nav.Link>
+              <Nav.Link href="/DSC">Sprint Cars</Nav.Link>
+              <Nav.Link href="/NASCAR">NASCAR</Nav.Link>
+              <Nav.Link href="/other">Other</Nav.Link>
+            </Nav>
+          </div>
       </Navbar>
   );
 }
