703599d2daefbdb784a618481d7cd5c6b410157a
codyallen9202 <codyman9202@gmail.com>
2023-12-10 14:30:18 -0500

    combined article card into single article

--- src/components/single-article.js
+++ src/components/single-article.js
@@ -1,7 +1,8 @@
-import SingleArticleCard from "./single-article-card";
 import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import { client } from "../client";
+import Card from 'react-bootstrap/Card';
+import SanityBlockContent from '@sanity/block-content-to-react';
 
 const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
@@ -47,17 +48,29 @@ const SingleArticle = () => {
 
     if (!singlePost) return <div>Loading...</div>;
 
+    const title=singlePost.title ? singlePost.title : 'Error: Title Not Found';
+    const image=singlePost.mainImage ? singlePost.mainImage.asset.url : null;
+    const author=singlePost.author ? singlePost.author.name : 'No Author Found';
+    const date=singlePost.publishedAt ? dateFormat(singlePost.publishedAt) : 'Null';
+    const tag=singlePost.categories ? singlePost.categories.title : 'Other';
+    const body=singlePost.body ? singlePost.body : null;
+
     return (
-        <div className='article-container'>
-            <SingleArticleCard
-                title={singlePost.title ? singlePost.title : 'Error: Title Not Found'}
-                image={singlePost.mainImage ? singlePost.mainImage.asset.url : null}
-                author={singlePost.author ? singlePost.author.name : 'No Author Found'}
-                date={singlePost.publishedAt ? dateFormat(singlePost.publishedAt) : 'Null'}
-                tag={singlePost.categories ? singlePost.categories.title : 'Other'}
-                body={singlePost.body ? singlePost.body : null}
-            />
-        </div>
+        <Card className='single-article'>
+            <Card.Img className='article-card-img' src={image}/>
+            <Card.Body>
+                <Card.Title className='article-card-title'>{title}</Card.Title>
+                <Card.Text className='article-card-author'>{author} | {date} | {tag}</Card.Text>
+                <Card.Text>
+                    <SanityBlockContent 
+                        blocks={body}
+                        projectId='xkb4ar5f'
+                        dataset='production'
+                        className='article-body'
+                        />
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
 
