7e03f59593de2007a6e7e2fdc1001b8025167b3f
codyallen9202 <codyman9202@gmail.com>
2023-12-10 14:37:10 -0500

    fixed ad

--- src/components/vertical-ad.js
+++ src/components/vertical-ad.js
@@ -8,7 +8,9 @@ function VerticalAd() {
                 <Card.Title className='vertical-ad-title'>OUR SPONSORS</Card.Title>
                 <Card.Body>
                     <div className='single-ad'>
-                        <Card.Img src='/images/hf-logo.jpg'/>
+                        <a href='https://www.huckfinnsrestaurant.com/' target='_blank' rel='noreferrer'>
+                            <Card.Img src='/images/hf-logo.jpg'/>
+                        </a>
                     </div>
                     <div className='single-ad'>
                         <Card.Text>Want to advertise with us?</Card.Text>
