3b2832dd80acaabdf1d42ad11447ea79f3d4b32c
codyallen9202 <codyman9202@gmail.com>
2025-02-22 08:44:58 -0500

    shortened headers to fit screen

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -4,24 +4,24 @@ import '../styles/styles.css'
 
 export default function HeaderBar() {
   return (
-      <Navbar className='header-bar' bg='dark' fixed='top'>
-          <div className='bar-wrapper'>
-            <Navbar.Brand href="/home">
-              <img
-                alt="Motorsports Retport"
-                src={'/images/white-logo.png'}
-                height={70}
-              />
-            </Navbar.Brand>
-            <Nav>
-              <Nav.Link href="/home">Home</Nav.Link>
-              <Nav.Link href="/DLM">Dirt Late Models</Nav.Link>
-              <Nav.Link href="/DSC">Sprint Cars</Nav.Link>
-              <Nav.Link href="/NASCAR">NASCAR</Nav.Link>
-              <Nav.Link href="/photos">Photos</Nav.Link>
-              <Nav.Link href="/other">Other</Nav.Link>
-            </Nav>
-          </div>
-      </Navbar>
+    <Navbar className='header-bar' bg='dark' fixed='top'>
+      <div className='bar-wrapper'>
+        <Navbar.Brand href="/home">
+          <img
+            alt="Motorsports Retport"
+            src={'/images/white-logo.png'}
+            height={70}
+          />
+        </Navbar.Brand>
+        <Nav>
+          <Nav.Link href="/home">Home</Nav.Link>
+          <Nav.Link href="/DLM">DLM</Nav.Link>
+          <Nav.Link href="/DSC">DSC</Nav.Link>
+          <Nav.Link href="/NASCAR">NASCAR</Nav.Link>
+          <Nav.Link href="/photos">Photos</Nav.Link>
+          <Nav.Link href="/other">Other</Nav.Link>
+        </Nav>
+      </div>
+    </Navbar>
   );
 }
